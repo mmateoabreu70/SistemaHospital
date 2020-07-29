5ece97681bd8fca6e5e8746f7477a0fd997c8c23
RoshbyHDZ <61295227+RoshbyHDZ@users.noreply.github.com>
2020-07-28 22:19:17 -0400

    Create ReporteSistema.php

---
+++ libreria/objetos/ReporteSistema.php
@@ -0,0 +1,45 @@
+<?php
+include_once("libreria/db/conexion.php");
+
+class ReporteSistema
+{
+    private $Id = 0;
+
+    function __get($name)
+    {
+        return $this->$name;
+    }
+
+    function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    function RegistrarEvento($idEvento, $idPaciente = "")
+    {
+
+        //Obteniendo fecha y hora
+        date_default_timezone_set('America/Santo_Domingo');
+        $fecha_hora = date("Y-m-d H:i:s");
+
+        //obteniendo usuario
+        $idUser = $_SESSION['id'];
+
+        $con = Conexion::getInstance();
+
+        if($idPaciente != null)
+        {
+            $query = "INSERT INTO reportesistema VALUES (0, '$fecha_hora', $idEvento, $idUser, '$idPaciente');";
+        } else {
+            $query = "INSERT INTO reportesistema VALUES (0, '$fecha_hora', $idEvento, $idUser, null);";
+        }
+        
+        if(mysqli_query($con, $query))
+        {
+
+            $this->Id = mysqli_insert_id($con);
+            return true;
+        }
+
+    }
+}
