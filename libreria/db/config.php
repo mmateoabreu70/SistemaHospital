4b6ea490a5c7331523657e29b55cfdfe543a0412
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2020-07-28 23:09:13 -0400

    Update config.php

--- libreria/db/config.php
+++ libreria/db/config.php
@@ -4,14 +4,16 @@
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "mysql");
+define("DB_PASS", "");
 define("DB_NAME", "sistemahospital");
-=======
+?>
+
 <?php
 //Esta es la configuracion de la base de datos
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "mysql");
-define("DB_NAME", "sistemahospital");
->>>>>>> b01b5e6261fea8937bb385005d135886267fb0b4:libreria/db/config.php
+//define("DB_HOST", "localhost");
+//define("DB_USER", "root");
+//define("DB_PASS", "");
+//define("DB_NAME", "sistemahospital");
+//>>>>>>> b01b5e6261fea8937bb385005d135886267fb0b4:libreria/db/config.php
+?>
