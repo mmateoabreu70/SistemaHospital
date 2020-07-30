f60b770d89c53e1e1dad5fcd9c13825e700aeaa3
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2020-07-29 22:15:16 -0400

    Update includes.php

--- libreria/includes.php
+++ libreria/includes.php
@@ -4,4 +4,5 @@ include_once("libreria/db/config.php");
 include_once("libreria/db/conexion.php");
 include_once("funciones.php");
 include_once("libreria/objetos/Usuario.php");
+include_once("libreria/objetos/ReporteSistema.php");
 
