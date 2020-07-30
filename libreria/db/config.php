b1207841cffb7b2246e5ef9e3c3e16b0d1eb4809
RoshbyHDZ <61295227+RoshbyHDZ@users.noreply.github.com>
2020-07-29 23:54:18 -0400

    Update config.php

--- libreria/db/config.php
+++ libreria/db/config.php
@@ -1,7 +1,7 @@
 <?php
 //Esta es la configuracion de la base de datos
 
-define("DB_HOST", "localhost");
+define("DB_HOST", "localhost:3307");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "sistemahospital");
