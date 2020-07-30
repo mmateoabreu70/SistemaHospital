9ffda609ea17e74bd2d1aaff64979ee6b6da9a32
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2020-07-30 02:55:24 -0400

    Update asignarCitas.php

--- asignarCitas.php
+++ asignarCitas.php
@@ -19,19 +19,37 @@ $conexion = Conexion::getInstance();
             <div class="form-row col-12 py-2">
                 <label class="input-group-addon" for="hora">Hora</label>
                 <input type="text" name="hora" id="hora" class="form-control"/>            
-            </div>
-           <!-- <div class="form-row col-12 py-2">
-                <label  for="cedula">Cedula</label>                                     
-                <input type="text" name="cedula" id="cedula"  class="form-control"/>            
-            </div>-->
+            </div>           
             <div class="form-row col-12 py-2">
                 <label class="input-group-addon" for="duracion">Duracion</label>
                 <input type="number" name="duracion" class="form-control"/>            
             </div>
-           <!-- <div class="form-row col-12 py-2">
-                <label class="input-group-addon" for="apellido">Apellido</label>
-                <input type="text" name="apellido" id="apellido" class="form-control"/>            
-            </div>-->
+            <div class="form-row col-12 py-2">
+                <label class="input-group-addon" for="cedula">Cedula</label>
+                <select name="cedula" class="form-control">                 
+                <option value="">Seleccione una cedula</option>
+               <?php                            
+                $query = "SELECT cedula from pacientes;";
+                $resultado = mysqli_query($conexion, $query);                
+                while($row=mysqli_fetch_array($resultado)){
+                ?>
+                <option value="<?php echo $row['cedula']?>"><?php echo $row['cedula'];?></option>
+                <?php } ?>
+                </select>                    
+            </div>
+            <div class="form-row col-12 py-2">
+                <label class="input-group-addon" for="medico">Medico</label>
+                <select name="medico" class="form-control">                 
+                <option value="">Seleccione medico</option>
+               <?php                            
+                $query = "SELECT nomUser from usuarios WHERE tipo = 3;";
+                $resultado = mysqli_query($conexion, $query);                
+                while($row=mysqli_fetch_array($resultado)){
+                ?>
+                <option value="<?php echo $row['nomUser']?>"><?php echo $row['nomUser'];?></option>
+                <?php } ?>
+                </select>                    
+            </div>        
            
             <!--<div class="form-row col-12 py-2">
                 <label class="input-group-addon" for="tipoSangre">Tipo de sangre</label>
