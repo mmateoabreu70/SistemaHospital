96e29775d056e1bf8222018392701cabdfd0d68c
Katherin Soto <katsoto0314@gmail.com>
2020-08-03 12:18:25 -0400

    Update Ver_Dinero.php
    Terminado :3

--- Ver_Dinero.php
+++ Ver_Dinero.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
-
 include('libreria/includes.php');
-
+//// Aqui es el select para la tabla de citas y asi mostrar algunos campos
 $conexion = Conexion::getInstance();
 $sql="SELECT id, paciente,fechaCita, costo from citas";
 $result = $conexion->query($sql);
+ /// Aqui es la suma de todos los campos de la celda
+$consulta="SELECT SUM(costo) as TotalPrecios FROM citas";
+$resultado=$conexion -> query($consulta);
+$fila=$resultado->fetch_assoc();
 
 if(isset($_GET['id']))
     {
@@ -13,7 +16,6 @@ if(isset($_GET['id']))
         $user->Id = $_GET['id'];
 
         $user->eliminarUsuario();
-        header("Location:AdminUsuario.php"); 
     }
 ?>
 <html>
@@ -31,7 +33,6 @@ if(isset($_GET['id']))
         </thead>
         <tbody>
             <?php
-
                 foreach($result as $fila)
                 {
                     echo "
@@ -40,32 +41,25 @@ if(isset($_GET['id']))
                             <td>{$fila['paciente']}</td>
                             <td>{$fila['fechaCita']}</td>
                             <td>{$fila['costo']}</td>
-
                         </tr>
                     ";
                 }
+                /// Aqui se esta haciendo el mostrar del total
 
+                foreach ($resultado as $fila) {
+                    $TotalPrecios=$fila['TotalPrecios'];
+
+                }
+
+                echo "<tr>
+                <th colspan ='3'></th>
+        <th>Total: $TotalPrecios </th>
+                </tr>              
+                "
             ?>
         </tbody>
     </table>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
 include('libreria/foot.php');
 ?>
