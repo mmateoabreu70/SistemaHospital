c430b90757b0d04ba230223338c3cdb9255f0d68
RoshbyHDZ <61295227+RoshbyHDZ@users.noreply.github.com>
2020-08-06 17:58:08 -0400

    xxx

---
+++ calendarioConfig.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$servidor='localhost:3307';
+$usuario='root';
+$pass='';
+$bd='sistemahospital';
+
+
+$conexion = new mysqli($servidor, $usuario, $pass, $bd);	
+
+
+$conexion->set_charset('utf8');
+
+
+if ($conexion->connect_errno) {
+	echo "Error al conectar la base de datos {$conexion->connect_errno}";
+}
+
+
+$base_url="http://localhost/sistemahospital/";
+
+?>
--- libreria/db/config.php
+++ libreria/db/config.php
@@ -1,7 +1,7 @@
-<?php
+<?phpsistemahospital
 //Esta es la configuracion de la base de datos
 
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "sistemahospital");
+define("DB_NAME", "");
