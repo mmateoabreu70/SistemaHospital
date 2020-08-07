66bf517c09bea671a902d1e727bfee78fedb2d8e
mmateoabreu70 <mmateoabreu70@gmail.com>
2020-08-07 04:41:53 -0400

    ult act

--- libreria/db/config.php
+++ libreria/db/config.php
@@ -1,9 +1,9 @@
 <?php
 //Esta es la configuracion de la base de datos
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "mysql");
-define("DB_NAME", "sistemahospital");
+define('DB_HOST', 'sql204.eshost.com.ar');
+define('DB_USER', 'eshos_26242027');
+define('DB_PASS', '3grb9d6c');
+define('DB_NAME', 'eshos_26242027_sistemahospital');
 
 ?>
